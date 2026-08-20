25418b9f489db48bd2099ff6d822390ffa97a1bd
Andrew1201-c <a39294929@gmail.com>
2026-08-20 11:02:07 +0400

    New code for writeautogen.py

--- Script/writeautogen.py
+++ Script/writeautogen.py
@@ -5,3 +5,46 @@
 #  Copyright (c) 2026 Andrew
 #  SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
+
+import configparser
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print("Usage: def2h <filename> <output.h>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+output = sys.argv[2]
+
+if not os.path.exists(filename):
+    print("The file does not exist: " + filename)
+    sys.exit(1)
+
+config = configparser.ConfigParser()
+config.optionxform = str
+config.read(filename)
+
+if "Defines" not in config:
+    print("Section does not exist: Defines")
+    sys.exit(1)
+
+with open(output, "w", encoding="utf-8") as f:
+
+    f.write("#ifndef AUTOGEN_H\n")
+    f.write("#define AUTOGEN_H\n\n")
+
+    for key, value in config["Defines"].items():
+
+        key = key.strip()
+        parts = value.split("|")
+
+        actual_value = parts[0].strip()
+        kind = parts[1].strip().lower() if len(parts) > 1 else None
+
+        if kind == "macro":
+            f.write(f"#define {key} {actual_value}\n")
+
+    f.write("\n#endif /* AUTOGEN_H */\n")
+
+print("Generated: " + output)
